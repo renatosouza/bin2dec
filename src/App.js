fc9059b44a0b41e1a1ae6378df60441ffbb67f39
Renato <renatosouzavv@gmail.com>
2020-05-27 17:13:42 -0300

    Adds the state to store the value entered and the result, and the button to make the conversion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-  const text = "110";
-  const binaryToDecimal = binary => {
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      binaryText: "111",
+      decimalNumber: 0,
+    }
+  }
+  
+  binaryToDecimal(binary) {
     // return parseInt(binary, 2);
     let decimal = 0;
     for(let i = binary.length-1; i>=0; i--){
       decimal += parseInt(binary[i])*Math.pow(2,binary.length-1-i);
     }
-    return decimal;
+    this.setState({decimalNumber: decimal})
   }
 
-  return (
-    <div className="App">
-      {text}
-      <br/>
-      {binaryToDecimal(text)}
-    </div>
-  );
+  render() {
+    const {binaryText, decimalNumber} = this.state;
+
+    return (
+      <div className="App">
+        {binaryText}
+        <br />
+        <button onClick={() => this.binaryToDecimal(binaryText)}>
+          Convert!
+        </button>
+        <br/>
+        {decimalNumber}
+      </div>
+    );
+  }
 }
 
 export default App;
