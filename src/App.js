792da5b5aa6fa6d0b7ae1ca85e78864628e0a04a
Renato <renatosouzavv@gmail.com>
2020-05-28 14:54:06 -0300

    Adds form with submit method, conversor working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// O ERRO TEM A VER COM O LANCE DO BIND
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      binaryText: "111",
+      binaryText: '',
       decimalNumber: 0,
-    }
+    };
   }
   
-  binaryToDecimal(binary) {
+  binaryToDecimal = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    const {binaryText} = this.state; 
     // return parseInt(binary, 2);
     let decimal = 0;
-    for(let i = binary.length-1; i>=0; i--){
-      decimal += parseInt(binary[i])*Math.pow(2,binary.length-1-i);
+    for(let i = binaryText.length-1; i>=0; i--){
+      decimal += parseInt(binaryText[i])*Math.pow(2,binaryText.length-1-i);
     }
     this.setState({decimalNumber: decimal})
   }
 
   render() {
     const {binaryText, decimalNumber} = this.state;
-
     return (
       <div className="App">
-        {binaryText}
-        <br />
-        <button onClick={() => this.binaryToDecimal(binaryText)}>
-          Convert!
-        </button>
+        <form onSubmit={this.binaryToDecimal}>
+          <input
+            type="text"
+            value={binaryText}
+            onChange={(event) => this.setState({binaryText: event.target.value})}
+          />
+          <button type="submit">
+            Convert!
+          </button>
+        </form>
         <br/>
         {decimalNumber}
       </div>
