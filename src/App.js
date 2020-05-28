12b57f3c5552ae6782c28ee708ff21d1256b6e84
Renato <renatosouzavv@gmail.com>
2020-05-28 15:58:58 -0300

    Converts the class component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-// O ERRO TEM A VER COM O LANCE DO BIND
-class App extends Component {
-  constructor(props){
-    super(props);
+const App = () => {
+  const [binaryText, setBinaryText] = useState("");
+  const [decimalNumber, setDecimalNumber] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    this.state = {
-      binaryText: "",
-      decimalNumber: 0,
-      errorMessage: "",
-    };
-  }
 
-  binaryToDecimal = (binary) => {
-    // const decimal = parseInt(binary, 2);
+  const binaryToDecimal = (binary) => {
     let decimal = 0;
     for(let i = binary.length-1; i>=0; i--){
       decimal += parseInt(binary[i])*Math.pow(2,binary.length-1-i);
     }
     return decimal;
   }
-  
-  onFormSubmit = (event) => {
+
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    const {binaryText} = this.state; 
-    // this.setState({decimalNumber: parseInt(binaryText, 2)});
+    // setDecimalNumber(parseInt(binaryText, 2));
     
     if((/^[01]*$/).test(binaryText)) {
-      const decimal = this.binaryToDecimal(binaryText);
-      this.setState({
-        decimalNumber: decimal,
-        errorMessage: "",
-      });
+      const decimal = binaryToDecimal(binaryText);
+      setDecimalNumber(decimal);
+      setErrorMessage("");
     } else {
-      this.setState({
-        decimalNumber: 0,
-        errorMessage: "Non-binary entry!"
-      });
+      setDecimalNumber(0);
+      setErrorMessage("Non-binary entry!");
     }
   }
 
-  render() {
-    const {binaryText, decimalNumber, errorMessage} = this.state;
-    return (
-      <div className="App">
-        {errorMessage}
-        <form onSubmit={this.onFormSubmit}>
-          <input
-            type="text"
-            value={binaryText}
-            onChange={(event) => this.setState({binaryText: event.target.value})}
-          />
-          <button type="submit">
-            Convert!
-          </button>
-        </form>
-        <br/>
-        {decimalNumber}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {errorMessage}
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={binaryText}
+          onChange={(event) => setBinaryText(event.target.value)}
+        />
+        <button type="submit">
+          Convert!
+        </button>
+      </form>
+      <br/>
+      {decimalNumber}
+    </div>
+  );
 }
 
 export default App;
